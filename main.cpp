cf6bdda6022aeba6c237fb391dbc21b1f61d0f07
irenemassa03 <114859857+irenemassa03@users.noreply.github.com>
2022-12-12 18:01:08 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+  int n;
+  cin>>n;
+  float a[n];
+  float x;
+  for(int i=0;i<n;i++){
+    cin>>x;
+    a[i]=x;
+  }
+  cout << valmax(a,n);
   return 0;
 }
